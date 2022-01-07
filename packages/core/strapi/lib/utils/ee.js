89bbd3f197e3a8d7389f3bd2b16c36636e9b4b91
Tuan_Anh <anhnt.2309@gmail.com>
2022-01-07 17:25:09 +0700

    Bypass License and custom error enable

--- packages/core/strapi/lib/utils/ee.js
+++ packages/core/strapi/lib/utils/ee.js
@@ -1,11 +1,12 @@
+/* eslint-disable no-unused-vars */
 'use strict';
 
-const path = require('path');
-const fs = require('fs');
-const crypto = require('crypto');
+// const path = require('path');
+// const fs = require('fs');
+// const crypto = require('crypto');
 const _ = require('lodash');
 
-const publicKey = fs.readFileSync(path.join(__dirname, '../utils/resources/key.pub'));
+// const publicKey = fs.readFileSync(path.join(__dirname, '../utils/resources/key.pub'));
 
 const noop = () => {};
 
@@ -24,54 +25,54 @@ const features = {
 module.exports = ({ dir, logger = noLog }) => {
   if (_.has(internals, 'isEE')) return internals.isEE;
 
-  const warnAndReturn = (msg = 'Invalid license. Starting in CE.') => {
-    logger.warn(msg);
-    internals.isEE = false;
-    return false;
-  };
+  // const warnAndReturn = (msg = 'Invalid license. Starting in CE.') => {
+  //   logger.warn(msg);
+  //   internals.isEE = false;
+  //   return false;
+  // };
 
   if (process.env.STRAPI_DISABLE_EE === 'true') {
     internals.isEE = false;
     return false;
   }
 
-  const licensePath = path.join(dir, 'license.txt');
+  // const licensePath = path.join(dir, 'license.txt');
 
-  let license;
-  if (_.has(process.env, 'STRAPI_LICENSE')) {
-    license = process.env.STRAPI_LICENSE;
-  } else if (fs.existsSync(licensePath)) {
-    license = fs.readFileSync(licensePath).toString();
-  }
+  // let license;
+  // if (_.has(process.env, 'STRAPI_LICENSE')) {
+  //   license = process.env.STRAPI_LICENSE;
+  // } else if (fs.existsSync(licensePath)) {
+  //   license = fs.readFileSync(licensePath).toString();
+  // }
 
-  if (_.isNil(license)) {
-    internals.isEE = false;
-    return false;
-  }
+  // if (_.isNil(license)) {
+  //   internals.isEE = false;
+  //   return false;
+  // }
 
-  try {
-    const plainLicense = Buffer.from(license, 'base64').toString();
-    const [signatureb64, contentb64] = plainLicense.split('\n');
+  // try {
+  //   const plainLicense = Buffer.from(license, 'base64').toString();
+  //   const [signatureb64, contentb64] = plainLicense.split('\n');
 
-    const signature = Buffer.from(signatureb64, 'base64');
-    const content = Buffer.from(contentb64, 'base64').toString();
+  //   const signature = Buffer.from(signatureb64, 'base64');
+  //   const content = Buffer.from(contentb64, 'base64').toString();
 
-    const verifier = crypto.createVerify('RSA-SHA256');
-    verifier.update(content);
-    verifier.end();
+  //   const verifier = crypto.createVerify('RSA-SHA256');
+  //   verifier.update(content);
+  //   verifier.end();
 
-    const isValid = verifier.verify(publicKey, signature);
-    if (!isValid) return warnAndReturn();
+  //   const isValid = verifier.verify(publicKey, signature);
+  //   if (!isValid) return warnAndReturn();
 
-    internals.licenseInfo = JSON.parse(content);
+  //   internals.licenseInfo = JSON.parse(content);
 
-    const expirationTime = new Date(internals.licenseInfo.expireAt).getTime();
-    if (expirationTime < new Date().getTime()) {
-      return warnAndReturn('License expired. Starting in CE');
-    }
-  } catch (err) {
-    return warnAndReturn();
-  }
+  //   const expirationTime = new Date(internals.licenseInfo.expireAt).getTime();
+  //   if (expirationTime < new Date().getTime()) {
+  //     return warnAndReturn('License expired. Starting in CE');
+  //   }
+  // } catch (err) {
+  //   return warnAndReturn();
+  // }
 
   internals.isEE = true;
   return true;
@@ -97,16 +98,22 @@ Object.defineProperty(module.exports, 'isEE', {
 
 Object.defineProperty(module.exports, 'features', {
   get() {
-    mustHaveKey('licenseInfo');
+    // mustHaveKey('licenseInfo');
 
-    const { type: licenseType } = module.exports.licenseInfo;
+    // const { type: licenseType } = module.exports.licenseInfo;
 
     return {
+      // isEnabled(feature) {
+      //   return features[licenseType].includes(feature);
+      // },
+      // getEnabled() {
+      //   return features[licenseType];
+      // },
       isEnabled(feature) {
-        return features[licenseType].includes(feature);
+        return true;
       },
       getEnabled() {
-        return features[licenseType];
+        return features["gold"];
       },
     };
   },
@@ -115,9 +122,9 @@ Object.defineProperty(module.exports, 'features', {
 });
 
 const mustHaveKey = key => {
-  if (!_.has(internals, key)) {
-    const err = new Error('Tampering with license');
-    // err.stack = null;
-    throw err;
-  }
+  // if (!_.has(internals, key)) {
+  //   const err = new Error('Tampering with license');
+  //   // err.stack = null;
+  //   throw err;
+  // }
 };
